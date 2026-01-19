6c4859d416eea124143a406d840505a03982b1a1
PoonamTecorb05 <poonam@tecorb.co>
2026-01-19 17:52:39 +0530

    modification

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/Teacher_AI_Agent/",
   server: {
     host: "::",
     port: 8080,
