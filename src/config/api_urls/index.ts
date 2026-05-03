9aa14591ecb7c871a195e6685e447a64126ff943
kumar-tinkesh <kumartinkesh32@gmail.com>
2026-05-03 13:14:09 +0530

    first time deployment

--- src/config/api_urls/index.ts
+++ src/config/api_urls/index.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = "https://api.tecorb.in";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 export const VERSION = "/api/v1";
 
 export const API_URL = {
